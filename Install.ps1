59116ae2a3c7664b779a1c1ca0a109aec4a06794
Marsu5 <marcell.schuh@gmail.com>
2017-05-23 19:56:36 +0200

    Fixes + Post Install script

--- Install.ps1
+++ Install.ps1
@@ -1,7 +1,7 @@
 $url = "http://boxstarter.org/package/nr/url?https://raw.githubusercontent.com/Marsu5/windows-development-environment/master/boxstarter"
 function setupprofile () {
     if(Test-Path $HOME\Documents\WindowsPowerShell\){
-        Copy-Item ./Microsoft.PowerShell-profile.ps1 $HOME\Documents\WindowsPowerShell\profile.ps1
+        Copy-Item .\Microsoft.PowerShell-profile.ps1 $HOME\Documents\WindowsPowerShell\profile.ps1
     }else{
         mkdir $HOME\Documents\WindowsPowerShell\
         setupprofile
@@ -21,10 +21,10 @@ if((Get-WmiObject Win32_OperatingSystem).OSArchitecture -eq "64-bit"){
     Write-Output "Installing software package."
     Write-Output "Seting up powershell profile."
     setupprofile
+    Copy-Item .\PostInstall.ps1 $env:temp\postinstall.ps1
     Write-Output "Installation complete. Press any key to quit ..."
     $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
 }else {
+    # Why are you even using a 32-bit OS in 2017?
     Write-Output "This script only supports 64-bit systems!"
 }
-
-
---
+++ PostInstall.ps1
@@ -0,0 +1,3 @@
+echo "Post install!!!!!!"
+Remove-Item "C:\Program Files (x86)\Gow\bin\ssh.bat"
+$host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
