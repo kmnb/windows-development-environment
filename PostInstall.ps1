51b5213285b3b9f83f3c1b4769cf7277839b9295
Marsu5 <marcell.schuh@gmail.com>
2017-05-25 16:17:26 +0200

    Changed packages

--- PostInstall.ps1
+++ PostInstall.ps1
@@ -1,2 +1,7 @@
 # Removes GOW SSH
 Remove-Item "C:\Program Files (x86)\Gow\bin\ssh.bat"
+
+# Install ninite package
+Write-Output "Installing ninite package..."
+Invoke-WebRequest "https://ninite.com/7zip-audacity-chrome-dropbox-googledrive-jdkx8-paint.net-qbittorrent-sharex-teamviewer12-vlc-windirstat/ninite.exe" -OutFile "ninite.exe"
+Start-Process ninite.exe
